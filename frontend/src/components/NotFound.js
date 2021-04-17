4039e173ee5b69c5f4d548eb1015c5b5d7f1385c
Jordan Handwerger <jordanhandwerger@yahoo.com>
2021-04-17 14:10:05 -0400

    added missing NotFound component

---
+++ frontend/src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = (props) => {
+    return (
+        <div>
+            <p>404 Error: Page Not Found</p>
+        </div>
+    );
+};
+
+export default NotFound;
