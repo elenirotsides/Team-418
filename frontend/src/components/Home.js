cdb8a94d77bc453a08fb1ac4b391c6a3cbc10cb4
unknown <patrick.sommer16@gmail.com>
2021-04-19 10:37:22 -0400

    adding IGDB singleton handler to manage IGDB oath token utility function for creating an IGDB API call to automatically include token headers

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,6 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Home = (props) => {
+
+    useEffect(() => {
+        fetch('http://localhost:5000/games', {
+            credentials: 'include',
+            mode:'cors'
+          }).then(res => res.json())
+            .then(
+                (result) => {
+                    alert(result.text);
+                },
+                (error) => {
+
+                }
+            )
+    }, []);
+
     return (
         <div>
             <p>Home</p>
