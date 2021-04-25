27a0bcb27bff7f6fbf1421adbb7b358be12eb119
zzwerling <zzwerlin@stevens.edu>
2021-04-25 19:08:25 -0400

    Updated useEffect hook in Profile.js

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import SignOutButton from './LogOut';
 import ChangePassword from './ChangePassword';
 
 const Profile = (props) => {
+    //call backend route and retrieve response
+    const [pageData, setPageData] = useState(undefined);
+    const loaded = false;
+    const email = 'bsanders@gmail.com';
+    const url = 'http://localhost:5000/users/profile/' + email;
+    useEffect(() => {
+        fetch(url, {
+           
+          }).then(res => res.json())
+            .then(
+                (result) => {
+                    setPageData(result);
+                },
+                (error) => {
+                    setPageData(error);
+                }
+            )
+    }, []);
+
+    //TO-DO: add stuff to profile later
     return (
         <div className="text-center">
             <h2>Profile Page</h2>
+            <h2>Name: </h2>
             <ChangePassword />
             <SignOutButton />
         </div>
     );
 };
+
 
 export default Profile;
