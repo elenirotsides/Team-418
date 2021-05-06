d0278fa80d23987d598e2b688b7dedb561cdec25
zzwerling <zzwerlin@stevens.edu>
2021-05-06 17:30:33 -0400

    Added some more code for Add to favorites button

--- api/routes/users.js
+++ api/routes/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const validation = require('../data/').validation;
 const usersData = require('../data').users;
+const { ObjectId } = require("mongodb");
+
 
 router.post('/profile', async function(req, res){
     let email = req.body.email;
@@ -20,6 +22,23 @@ router.post('/profile', async function(req, res){
 
 });
 
+router.post('/addfavorites', async function(req, res){
+    let userId = req.body.userId;
+    let gameList = req.body.gameList;
 
+    let objId = new ObjectId(userId)
+    if (!ObjectId.isValid(objId)){
+        res.status(400).send("Invalid Object ID!");
+    }
+    try{
+        let favorites = await usersData.addFavorites(userId, gameList);
+        res.send(favorites);
+
+    }catch(error){
+        console.log(error);
+        res.status(500).send(error);
+    }
+
+});
 
 module.exports = router;
--- frontend/src/components/Game.js
+++ frontend/src/components/Game.js
@@ -6,13 +6,37 @@ import Button from '@material-ui/core/Button';
 const Game = (props) => {
     const [loading, setLoading] = useState(true);
     const [pageData, setPageData] = useState(undefined);
+    const [userData, setUserData] = useState(undefined);
     const [favText, setFavText] = useState("Add to Favorites");
     let idToken;
+    const email = "donotpassgo@gmail.com";
      
 
     async function addRemoveFavorites(){
+       
+
+        const url = "http://localhost:5000/users/addfavorites";
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: { userId: userData.toString(), gameList: [pageData._id]}
+        };
+        requestOptions.body.idToken = await getUserIdToken();
+        requestOptions.idToken = await getUserIdToken();
+        requestOptions.body = JSON.stringify(requestOptions.body);
         if (favText == "Add to Favorites"){
             setFavText("Remove From Favorites");
+            fetch(url, requestOptions)
+            .then(res => res.json())
+                .then(
+                    (result) => {
+                        console.log("Success");
+                    },
+                    (error) => {
+                        console.log("Error: " + error);
+                    }
+                )
+           
         }else{
             setFavText("Add to Favorites");
         }
@@ -22,6 +46,30 @@ const Game = (props) => {
 
 
     async function fetchData() {
+
+        const userUrl = 'http://localhost:5000/users/profile';
+
+        const userRequestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: { email: email},
+        };
+       userRequestOptions.body.idToken = await getUserIdToken();
+       userRequestOptions.idToken = await getUserIdToken();
+       userRequestOptions.body = JSON.stringify(userRequestOptions.body);
+        fetch(userUrl, userRequestOptions)
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    setUserData(result._id);
+                },
+                (error) => {
+                    console.log(error);
+                    setUserData(error);
+                }
+            );
+    
+
         if (!idToken) idToken = await getUserIdToken();
 
         fetch(
