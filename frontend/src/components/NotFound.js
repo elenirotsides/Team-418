e92c1b21b9ddbc940808ae248a74848f9618a56f
Zach Zwerling <zzwerlin@stevens.edu>
2021-05-17 19:15:39 -0400

    Not Found page enhanced

--- frontend/src/components/NotFound.js
+++ frontend/src/components/NotFound.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import logo from '../notFound404.gif';
+import { makeStyles, Typography } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
+
+const styles = makeStyles({
+    notFound: {
+        display: 'flex',
+        justifyContent: 'center'
+        },
+
+    notFoundImg: {
+        display: 'block',
+        margin: '0 auto'
+    }
+});
 
 const NotFound = (props) => {
+    const classes = styles();
     return (
-        <div>
-            <p>404 Error: Page Not Found</p>
+        <div >
+        <Typography class={classes.notFound}  variant="h1" gutterBottom>
+        Error
+      </Typography>
+      {(<img class={classes.notFoundImg} src={logo} alt="Error 404 Not Found" />
+) || <p>"Error 404, resource not found"</p>}
+
         </div>
     );
 };
